3d8419f919930ed625b7888c99c66d18be422de6
codessbabe <oana.anghelachi@gmail.com>
2020-10-14 10:51:20 +0300

    Create 'roll the dice' functionality for active player

--- app.js
+++ app.js
@@ -37,9 +37,22 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   diceDOM.style.display = 'block'; 
   diceDOM.src = 'dice-' + dice + '.png';
 
-
   // 3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) { 
+    // Add score
+    roundScore += dice; 
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else { 
+    // Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0; 
 
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+  }
 });
 
 
